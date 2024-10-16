c5429fda5f9da73e75c5300a17e319ab2a1d8b94
29anhthu <155529783+29anhthu@users.noreply.github.com>
2024-10-16 17:42:05 +0700

    Add files via upload

---
+++ DB_DANGKY.sql
@@ -0,0 +1,17 @@
+create database dbdangky
+GO
+use dbdangky
+GO
+CREATE TABLE KHACHHANG
+(
+MaKH INT IDENTITY(1,1),
+HoTen NVARCHAR(50) NOT NULL,
+Taikhoan Varchar(50) UNIQUE,
+Matkhau Varchar(50) NOT NULL,
+Email Varchar(100) UNIQUE,
+DiachiKH NVARCHAR(200),
+DienthoaiKH Varchar(50),
+Ngaysinh DATETIME
+CONSTRAINT PK_Khachhang PRIMARY KEY(MaKH)
+)
+select *from KHACHHANG
